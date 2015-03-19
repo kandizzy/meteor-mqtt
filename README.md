5f3c5b9af3cf51a66fda6b71f72c198cf19cf6af
Carrie Kengle <carriesteady@gmail.com>
2015-03-19 16:27:13 -0400

    upated package.js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# spectrum:mqtt
+# a fork of spectrum:mqtt
 [MQTT NPM Library] (https://github.com/mqttjs/MQTT.js) for Meteor
--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
-  name: 'spectrum:mqtt',
-  version: '0.0.3',
-  summary: 'MQTT library for Meteor',
-  git: 'https://github.com/acidsound/meteor-mqtt.git',
+  name: 'kandizzy:meteor-mqtt-fork',
+  version: '0.0.1',
+  summary: 'a fork of spectrum:mqtt',
+  git: 'https://github.com/kandizzy/meteor-mqtt',
   documentation: 'README.md'
 });
 
-Npm.depends({
-  "mqtt": "1.0.8"
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.1');
+  api.addFiles('mqtt.js');
 });
 
-Package.onUse(function(api) {
+Package.onTest(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('mqtt.js', 'server');
   api.export('mqtt');
