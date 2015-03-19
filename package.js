eb60e9b4f87d63bbd27875ef8890d502e1d61111
Carrie Kengle <carriesteady@gmail.com>
2015-03-19 16:43:01 -0400

    add dependency

--- package.js
+++ package.js
@@ -1,14 +1,20 @@
 Package.describe({
   name: 'kandizzy:meteor-mqtt-fork',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'a fork of spectrum:mqtt',
   git: 'https://github.com/kandizzy/meteor-mqtt',
   documentation: 'README.md'
 });
 
+
+Npm.depends({
+  "mqtt": "1.1.2"
+});
+
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.4.1');
-  api.addFiles('mqtt.js');
+  api.versionsFrom('1.0');
+  api.addFiles('mqtt.js', 'server');
+  api.export('mqtt');
 });
 
 Package.onTest(function(api) {
